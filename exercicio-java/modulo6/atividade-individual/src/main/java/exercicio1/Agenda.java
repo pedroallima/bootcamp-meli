3010ea23e7bcec9fd486732f568c052c298a1231
pedroallima <pedro.leao@mercadolivre.com>
2021-09-16 10:59:51 -0300

    Ëstruturando projeto indiidual

--- exercicio-java/modulo6/atividade-individual/src/main/java/exercicio1/Agenda.java
+++ exercicio-java/modulo6/atividade-individual/src/main/java/exercicio1/Agenda.java
@@ -8,26 +8,68 @@ public class Agenda {
 
     ////Recebeumobjetodotipopessoaeadiciona a pessoa na lista
     public void armazenarPessoa(Pessoa pessoa){
-
+        pessoas.add(pessoa);
     }
     ////Recebeumnome,busca o elemento na lista e remove a pessoa caso ela exista
+
     public void removerPessoa(String nome){
-
+        for(Pessoa pessoa: pessoas){
+            if (pessoa.getNome().equals(nome)){
+                pessoas.remove(nome);
+                System.out.println("Pessoa de nome: "+pessoa.getNome() + " REMOVIDO");
+                return;
+            }
+        }
+        System.out.println("Esta PESSOA NÃO FOI ENCONTRADA");
     }
+
+
     //// informa em que posiçãoda agenda está a pessoa
-    public int buscarPessoa(String nome){
 
+    public void buscarPessoa(String nome){
+        for(Pessoa pessoa: pessoas){
+            if (pessoa.getNome().equals(nome)){
+                System.out.println("Pessoa de nome: "+pessoa.getNome() + " ENCONTRADA");
+                return;
+            }
+        }
+        System.out.println("ESTA PESSOA NÃO FOI ENCONTRADA");
     }
+
+
     // exibe todos os dados detodas as pessoas da agenda
     public void exibirTodaAgenda(){
-
+        System.out.println("*******PESSOAS AGENDADAS***********");
+        for(Pessoa pessoa: pessoas){
+            System.out.println(pessoa.getNome());
+        }
     }
+
     //exibe todos os dados da pessoa que está na posição “index” da agenda na console
 
     void exibirPessoa(int index){
-
+        for(Pessoa pessoa: pessoas){
+            if (pessoas.get(index) != null ){
+                System.out.println("Nome: "+pessoas.get(index).nome+ " Endereco: "+pessoas.get(index).nome+" Telefone: "+pessoas.get(index).telefone+" Idqade: "+pessoas.get(index).idqade+" Alutira: "+pessoas.get(index).altura);
+                return;
+            }
+        }
+        System.out.println("ESTA PESSOA NÃO FOI ENCONTRADA");
     }
 
+    public void listarPessoasEmOrdemAlfabéticaDeNome(){
+        pessoas.stream().sorted((Pessoa p1, Pessoa p2) -> p1.getNome().compareTo(p2.getNome()));
+        pessoas.forEach(System.out::println);
+    }
+    public void listarPessoasEmOrdemAlfabeticaDeEndereco(){
+        pessoas.stream().sorted((Pessoa p1, Pessoa p2) -> p1.getEndereco().compareTo(p2.getEndereco()));
+        pessoas.forEach(System.out::println);
 
+    }
+    public void listarPessoasPorIdade(){
+        pessoas.stream().sorted((Pessoa p1, Pessoa p2) -> p1.getIdqade().compareTo(p2.getIdqade()));
+        pessoas.forEach(System.out::println);
+
+    }
 
 }
--- exercicio-java/modulo6/atividade-individual/src/main/java/exercicio1/exercicio1.java
+++ exercicio-java/modulo6/atividade-individual/src/main/java/exercicio1/exercicio1.java
@@ -3,10 +3,10 @@ package exercicio1;
 public class exercicio1 {
     /*Em sua classe principal:
     ● crie 10 pessoas e adicione as mesmas na agenda.
-    ●remova uma pessoa da lista
-    ●busque uma pessoa na lista pelo seu nome;
-    ●apresente todas as pessoas que estão na agenda;
-    ●apresente uma pessoa em específico;
+    ● remova uma pessoa da lista
+    ● busque uma pessoa na lista pelo seu nome;
+    ● apresente todas as pessoas que estão na agenda;
+    ● apresente uma pessoa em específico;
     OBS: utilize os métodos da classe Agenda para estas operações;*/
 
 /*Plus
@@ -17,13 +17,50 @@ public class exercicio1 {
 
 
     public static void main(String[] args) {
+      Agenda agenda = new Agenda();
+
+      //crie 10 pessoas e adicione as mesmas na agenda.
+      Pessoa pessoa = new Pessoa("Pedro","rua cerejeira","87988264086",35,1.70);
+      Pessoa pessoa2 = new Pessoa("Pedro2","rua cerejeira2","87988264081",32,1.70);
+      Pessoa pessoa3 = new Pessoa("Pedro3","rua cerejeira3","87988264082",33,1.70);
+      Pessoa pessoa4 = new Pessoa("Pedro4","rua cerejeira4","87988264083",34,1.70);
+      Pessoa pessoa5 = new Pessoa("Pedro5","rua cerejeira5","87988264084",37,1.70);
+      Pessoa pessoa6 = new Pessoa("Pedro6","rua cerejeira6","87988264085",39,1.70);
+      Pessoa pessoa7 = new Pessoa("Pedro7","rua cerejeira7","879882640866",32,1.70);
+      Pessoa pessoa8 = new Pessoa("Pedro8","rua cerejeira7","87988264087",30,1.70);
+      Pessoa pessoa9 = new Pessoa("Pedro9","rua cerejeira9","87988264088",21,1.70);
+      Pessoa pessoa10 = new Pessoa("Pedro10","rua cerejeira10","879882640869",40,1.70);
 
 
+      //Adicionando as pessoas na lista
+      agenda.armazenarPessoa(pessoa);
+      agenda.armazenarPessoa(pessoa2);
+      agenda.armazenarPessoa(pessoa3);
+      agenda.armazenarPessoa(pessoa4);
+      agenda.armazenarPessoa(pessoa5);
+      agenda.armazenarPessoa(pessoa6);
+      agenda.armazenarPessoa(pessoa7);
+      agenda.armazenarPessoa(pessoa8);
+      agenda.armazenarPessoa(pessoa9);
+      agenda.armazenarPessoa(pessoa10);
 
+      //remova uma pessoa da lista
+      agenda.removerPessoa("Pedro");
+      // busque uma pessoa na lista pelo seu nome;
+      agenda.buscarPessoa("Pedro2");
+      //apresente todas as pessoas que estão na agenda;
+      agenda.exibirTodaAgenda();
+      //apresente uma pessoa em específico;
+      agenda.exibirPessoa(1);
 
-       // listarPessoasEmOrdemAlfabéticaDeNome()
-        //listarPessoasEmOrdemAlfabeticaDeEndereco()
-        //listarPessoasPorIdade()
+      System.out.println("-----------------------------Ordenando por ordem alfabetica-------------------------------");
+        agenda.listarPessoasEmOrdemAlfabéticaDeNome();
+
+      System.out.println("-----------------------------Ordenando por ordem alfabetica-------------------------------");
+      agenda.listarPessoasEmOrdemAlfabeticaDeEndereco();
+
+      System.out.println("---------------------------------Ordenando por ordem idade--------------------------------");
+      agenda.listarPessoasPorIdade();
     }
 
 }
